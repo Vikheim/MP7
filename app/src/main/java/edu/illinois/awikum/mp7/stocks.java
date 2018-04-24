8caaeee473d6ed1c5d66020dcdc853d889c379ff
awikum <awikum2@illinois.edu>
2018-04-24 13:30:33 -0500

    AutoCompleteTextView finished

--- app/src/main/java/edu/illinois/awikum/mp7/stocks.java
+++ app/src/main/java/edu/illinois/awikum/mp7/stocks.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -27,6 +31,25 @@ public class stocks extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.stocks, container, false);
+
+        String COUNTRIES[]={"INDIA","ITALY","JAPAN","USA","ICELAND","INDONESIA","UK","IRAN","IRAQ"};
+        final AutoCompleteTextView autoCompleteTextViewCountry = (AutoCompleteTextView)rootView.findViewById(R.id.countries_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+
+        autoCompleteTextViewCountry.setAdapter(adapter);
+        autoCompleteTextViewCountry.setThreshold(1);
+        //textView tv = (textView)findViewbyid(R.id.)
+/*
+        btnSelectedCountry.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                String country=autoCompleteTextViewCountry.getText().toString();
+                textViewSelectedCountry.setText("Selected Country: "+country);
+            }
+        });
+        */
         return rootView;
     }
 }
