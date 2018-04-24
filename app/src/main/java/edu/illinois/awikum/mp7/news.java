5a7b3790c19ef82237751bf1b823e4854442a20f
awikum <awikum2@illinois.edu>
2018-04-23 23:30:03 -0500

    Initial commit

---
+++ app/src/main/java/edu/illinois/awikum/mp7/news.java
@@ -0,0 +1,17 @@
+package edu.illinois.awikum.mp7;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class news extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.news, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/edu/illinois/awikum/mp7/stocks.java
@@ -0,0 +1,16 @@
+package edu.illinois.awikum.mp7;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class stocks extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.stocks, container, false);
+        return rootView;
+    }
+}
